85c85cd3bba91f7021ab3976e4a8023fc6e1ffd6
Michał Bastrzyk <michal.bastrzyk.pl@gmail.com>
2026-01-06 16:36:22 +0100

    Initial Commit

---
+++ FlowClone/FlowCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowCloneApp.swift
+//  FlowClone
+//
+//  Created by Michał Bastrzyk on 06/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct FlowCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
